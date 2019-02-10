e0955ffb5e04e2ff296705f1672bf48d98f54dc6
David Squire <squiredd@gmail.com>
2019-02-09 22:43:16 -0600

    removed some unused code

--- sketch_rgb/sketch_rgb.ino
+++ sketch_rgb/sketch_rgb.ino
@@ -68,49 +68,13 @@ pinMode(GREEN_PIN, OUTPUT);
 pinMode(BLUE_PIN, OUTPUT);
 
 pinMode(DIR, INPUT);
-Serial.begin(9600);
 attachInterrupt(digitalPinToInterrupt(CLK),CW_Test,FALLING);
 }
 
 
 void loop()
 {
-// In this sketch, we'll start writing our own functions.
-// This makes the sketch easier to follow by dividing up
-// the sketch into sections, and not having everything in
-// setup() or loop().
-
-// We'll show you two ways to run the RGB LED.
-
-// The first way is to turn the individual LEDs (red, blue,
-// and green) on and off in various combinations. This gives you
-// a total of eight colors (if you count "black" as a color).
-
-// We've written a function called mainColors() that steps
-// through all eight of these colors. We're only "calling" the
-// function here (telling it to run). The actual function code
-// is further down in the sketch.
-
-//mainColors();
-
-// The above function turns the individual LEDs full-on and
-// full-off. If you want to generate more than eight colors,
-// you can do so by varying the brightness of the individual
-// LEDs between full-on and full-off.
-
-// The analogWrite() function lets us do this. This function
-// lets you dim a LED from full-off to full-on over 255 steps.
-
-// We've written a function called showSpectrum() that smoothly
-// steps through all the colors. Again we're just calling it
-// here; the actual code is further down in this sketch.
-
-//showSpectrum();
-
-  Serial.println(DT);
-  Serial.println("\n");
-  Serial.println(count);
-  delay(200);
+  
 }
 const int increment = 10; 
 void CW_Test(){
@@ -134,158 +98,6 @@ void CW_Test(){
   }
   showRGB(count);
 }
-
-
-// Here's the mainColors() function we've written.
-
-// This function displays the eight "main" colors that the RGB LED
-// can produce. If you'd like to use one of these colors in your 
-// own sketch, you cancopy and paste that section into your code.
-
-void mainColors()
-{
-// Off (all LEDs off):
-
-digitalWrite(RED_PIN, LOW);
-digitalWrite(GREEN_PIN, LOW);
-digitalWrite(BLUE_PIN, LOW);
-
-delay(1000);
-
-// Red (turn just the red LED on):
-
-digitalWrite(RED_PIN, HIGH);
-digitalWrite(GREEN_PIN, LOW);
-digitalWrite(BLUE_PIN, LOW);
-
-delay(1000);
-
-// Green (turn just the green LED on):
-
-digitalWrite(RED_PIN, LOW);
-digitalWrite(GREEN_PIN, HIGH);
-digitalWrite(BLUE_PIN, LOW);
-
-delay(1000);
-
-// Blue (turn just the blue LED on):
-
-digitalWrite(RED_PIN, LOW);
-digitalWrite(GREEN_PIN, LOW);
-digitalWrite(BLUE_PIN, HIGH);
-
-delay(1000);
-
-// Yellow (turn red and green on):
-
-digitalWrite(RED_PIN, HIGH);
-digitalWrite(GREEN_PIN, HIGH);
-digitalWrite(BLUE_PIN, LOW);
-
-delay(1000);
-
-// Cyan (turn green and blue on):
-
-digitalWrite(RED_PIN, LOW);
-digitalWrite(GREEN_PIN, HIGH);
-digitalWrite(BLUE_PIN, HIGH);
-
-delay(1000);
-
-// Purple (turn red and blue on):
-
-digitalWrite(RED_PIN, HIGH);
-digitalWrite(GREEN_PIN, LOW);
-digitalWrite(BLUE_PIN, HIGH);
-
-delay(1000);
-
-// White (turn all the LEDs on):
-
-digitalWrite(RED_PIN, HIGH);
-digitalWrite(GREEN_PIN, HIGH);
-digitalWrite(BLUE_PIN, HIGH);
-
-delay(1000);
-}
-
-
-// Below are two more functions we've written,
-// showSpectrum() and showRGB().
-
-// showRGB() displays a single color on the RGB LED.
-// You call showRGB() with the number of a color you want
-// to display.
-
-// showSpectrum() steps through all the colors of the RGB LED,
-// displaying a rainbow. showSpectrum() actually calls showRGB()
-// over and over to do this.
-
-// We'll often break tasks down into individual functions like
-// this, which makes your sketches easier to follow, and once
-// you have a handy function, you can reuse it in your other
-// programs.
-
-
-// showSpectrum()
-
-// This function steps through all the colors of the RGB LED.
-// It does this by stepping a variable from 0 to 768 (the total
-// number of colors), and repeatedly calling showRGB() to display
-// the individual colors.
-
-// In this function, we're using a "for() loop" to step a variable
-// from one value to another, and perform a set of instructions
-// for each step. For() loops are a very handy way to get numbers
-// to count up or down.
-
-// Every for() loop has three statements separated by semicolons:
-
-//   1. Something to do before starting
-
-//   2. A test to perform; as long as it's true,
-//      it will keep looping
-
-//   3. Something to do after each loop (usually
-//      increase a variable)
-
-// For the for() loop below, these are the three statements:
-
-//   1. x = 0;     Before starting, make x = 0.
-
-//   2. x < 768;   While x is less than 768, run the
-//                 following code.
-
-//   3. x++        Putting "++" after a variable means
-//                 "add one to it". (You can also use "x = x + 1")
-
-// Every time you go through the loop, the statements following
-// the loop (those within the brackets) will run.
-
-// And when the test in statement 2 is finally false, the sketch
-// will continue.
-
-
-void showSpectrum()
-{
-int x;  // define an integer variable called "x"
-
-// Now we'll use a for() loop to make x count from 0 to 767
-// (Note that there's no semicolon after this line!
-// That's because the for() loop will repeat the next
-// "statement", which in this case is everything within
-// the following brackets {} )
-
-for (x = 0; x < 768; x++)
-
-// Each time we loop (with a new value of x), do the following:
-
-{
-showRGB(x);  // Call RGBspectrum() with our new x
-delay(DISPLAY_TIME);   // Delay for 10 ms (1/100th of a second)
-}
-}
-
 
 // showRGB()
 
